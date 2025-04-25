2ffeff9d9f76ca328497622fa519fc13ee214ff0
Dracosomething <larsfalk08@gmail.com>
2025-04-25 14:04:07 +0200

    added timer class

---
+++ src/main/java/io/github/dracosomething/awakened_lib/library/ClientTickingObject.java
@@ -0,0 +1,16 @@
+package io.github.dracosomething.awakened_lib.library;
+
+import net.minecraft.world.phys.Vec3;
+
+import java.util.UUID;
+import java.util.logging.Level;
+
+public abstract class ClientTickingObject {
+    private UUID uuid;
+    private Level level;
+    private Vec3 position;
+    private int life;
+    private final Timer ticker;
+
+    public ClientTickingObject
+}
