9a9c5c7bed428712651542e039c261f5a1bd316b
Dracosomething <larsfalk08@gmail.com>
2025-05-14 16:07:53 +0200

    more networking

--- src/main/java/io/github/dracosomething/awakened_lib/network/p2c/SyncObjectsCapability.java
+++ src/main/java/io/github/dracosomething/awakened_lib/network/p2c/SyncObjectsCapability.java
@@ -1,18 +1,24 @@
 package io.github.dracosomething.awakened_lib.network.p2c;
 
 import io.github.dracosomething.awakened_lib.capability.IObjects;
+import io.github.dracosomething.awakened_lib.network.Packet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.network.CustomPayloadEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkContext;
+import net.minecraftforge.network.packets.Acknowledge;
 
 import java.util.function.Supplier;
 
-public class SyncObjectsCapability {
+public class SyncObjectsCapability extends Packet<SyncObjectsCapability> {
+    public static StreamCodec<FriendlyByteBuf, SyncObjectsCapability> STREAM_CODEC = StreamCodec.ofMember(SyncObjectsCapability::toBytes, SyncObjectsCapability::new);
     private final CompoundTag tag;
     private final ResourceKey<Level> level;
 
@@ -31,14 +37,18 @@ public class SyncObjectsCapability {
         buf.writeResourceKey(this.level);
     }
 
-    public void handle(Supplier<NetworkContext> ctx) {
+    public void handle(Supplier<CustomPayloadEvent.Context> ctx) {
         ctx.get().enqueueWork(() -> {
             DistExecutor.unsafeRunWhenOn(Dist.CLIENT, () -> {
                 return () -> {
-                    ClientAccess.updatePlayerCapability(this.entityId, this.tag);
+                    ServerPlayer player = ctx.get().getSender();
                 };
             });
         });
-        ((NetworkEvent.Context)ctx.get()).setPacketHandled(true);
+        ctx.get().setPacketHandled(true);
+    }
+
+    public StreamCodec<FriendlyByteBuf, SyncObjectsCapability> getCoded() {
+        return STREAM_CODEC;
     }
 }
