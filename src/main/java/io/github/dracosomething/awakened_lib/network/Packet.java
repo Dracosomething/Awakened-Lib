6c0b0d8dd056c04f819db3129168e17defcbc2ad
Dracosomething <larsfalk08@gmail.com>
2025-05-15 09:08:46 +0200

    created neoforge version

--- src/main/java/io/github/dracosomething/awakened_lib/network/Packet.java
+++
@@ -1,8 +0,0 @@
-package io.github.dracosomething.awakened_lib.network;
-
-import net.minecraft.network.FriendlyByteBuf;
-import net.minecraft.network.codec.StreamCodec;
-
-public abstract class Packet<T> {
-    public abstract StreamCodec<FriendlyByteBuf, T> getCoded();
-}
